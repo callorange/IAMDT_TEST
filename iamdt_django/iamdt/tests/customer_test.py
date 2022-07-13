2c8da8516a05a47c501ea9b5e2f0d1d103cd96b7
jeongyeob <sjy20000@gmail.com>
2022-07-13 17:21:49 +0900

    변경된 모델명에 맞춰 테스트 코드 수정 및 테스트코드 분할

--- iamdt_django/iamdt/tests/customer_test.py
+++ iamdt_django/iamdt/tests/customer_test.py
@@ -1,4 +1,4 @@
-__all__ = ["IAMDTCustomerModelTestCase"]
+__all__ = ["CustomerModelTestCase"]
 
 from django.core.exceptions import ValidationError
 from django.test import TestCase
@@ -6,14 +6,14 @@ from django.test import TestCase
 from iamdt.models import Customer
 
 test_customer_info = [
-    {"name": "고객1", "phone": "01011112222", "email": "customer@customer.com"},
-    {"name": "고객2", "phone": "01033334444", "email": "customer@customer.com"},
-    {"name": "고객3", "phone": "01055556666", "email": "customer@customer.com"},
-    {"name": "고객4", "phone": "01077778888", "email": "customer@customer.com"},
+    {"name": "고객", "phone": "01011112222"},
+    {"name": "고객2", "phone": "01033334444"},
+    {"name": "고객3", "phone": "01055556666"},
+    {"name": "고객", "phone": "01077778888"},
 ]
 
 
-class IAMDTCustomerModelTestCase(TestCase):
+class CustomerModelTestCase(TestCase):
     """
     고객 모델 테스트
     """
@@ -23,20 +23,67 @@ class IAMDTCustomerModelTestCase(TestCase):
         for customer_info in test_customer_info:
             Customer.objects.create(**customer_info)
 
-    def test_customer_info(self):
+    def test_unique_constraint(self) -> None:
+        """고객정보는 복합키(연락처+이름)를 사용하므로 해당 정보 확인"""
+        with self.assertRaises(Exception):
+            Customer.objects.create(**test_customer_info[0])
+
+    def test_info(self) -> None:
         """생성된 정보 확인"""
         customer_info = test_customer_info[0]
         customer = Customer.objects.get(
             name=customer_info["name"], phone=customer_info["phone"]
         )
 
-        # 연락처 확인
-        self.assertEqual(customer.phone, customer_info["phone"])
-
         # 이름 확인
         self.assertEqual(customer.name, customer_info["name"])
 
-    def test_unique_constraint(self):
-        """고객정보는 복합키(연락처+이름)를 사용하므로 해당 정보 확인"""
-        with self.assertRaises(Exception):
-            Customer.objects.create(**test_customer_info[0])
+        # 연락처 확인
+        self.assertEqual(customer.phone, customer_info["phone"])
+
+    def test_filter(self) -> None:
+        """고객 모델 검색"""
+        # 생성된 고객수는 4명
+        self.assertEqual(4, Customer.objects.all().count())
+
+    def test_filter_name(self) -> None:
+        """고객 모델 이름 검색"""
+        # 이름이 같은 고객은 2명
+        self.assertEqual(2, Customer.objects.filter(name="고객").count())
+
+    def test_filter_phone(self) -> None:
+        """고객 모델 이름 검색"""
+        # 전화번호는 중복이 없다.
+        self.assertEqual(1, Customer.objects.filter(phone="01011112222").count())
+
+    def test_validation_name(self) -> None:
+        """고객 정보 이름 검증"""
+        customer = Customer.objects.first()
+
+        customer.name = ""
+        with self.assertRaises(ValidationError):
+            customer.full_clean()
+
+        customer.name = None
+        with self.assertRaises(ValidationError):
+            customer.full_clean()
+
+    def test_validation_phone(self) -> None:
+        """고객 정보 연락처 검증"""
+        customer = Customer.objects.first()
+
+        customer.phone = "ㅁㅁㅁㅁ"
+        with self.assertRaises(ValidationError):
+            customer.full_clean()
+
+        customer.phone = "00012341234"
+        with self.assertRaises(ValidationError):
+            customer.full_clean()
+
+        customer.phone = None
+        with self.assertRaises(ValidationError):
+            customer.full_clean()
+
+        customer.phone = ""
+        with self.assertRaises(ValidationError):
+            customer.full_clean()
--- iamdt_django/iamdt/tests/uset_test.py
+++ iamdt_django/iamdt/tests/uset_test.py
@@ -1,4 +1,4 @@
-__all__ = ["IAMDTUserChoicesTestCase", "IAMDTUserModelTestCase"]
+__all__ = ["UserChoicesTestCase", "UserModelTestCase"]
 
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import check_password
@@ -51,7 +51,7 @@ user_type = user_model.UserType
 messenger_type = user_model.MessengerType
 
 
-class IAMDTUserChoicesTestCase(TestCase):
+class UserChoicesTestCase(TestCase):
     """
     유저 모델에서 사용할 TextChoices 서브클래스 테스트
     IAMDTUser.UserType: Role 종류
@@ -74,13 +74,13 @@ class IAMDTUserChoicesTestCase(TestCase):
             messenger_type.names,
         )
         self.assertEqual(
-            ["", "kakaotalk", "facebook", "line", "telegram"],
+            ["-", "kakaotalk", "facebook", "line", "telegram"],
             messenger_type.values,
         )
         self.assertEqual(["없음", "카카오톡", "페이스북", "라인", "텔레그램"], messenger_type.labels)
 
 
-class IAMDTUserModelTestCase(TestCase):
+class UserModelTestCase(TestCase):
     """
     유저 모델 테스트 클래스
     Django User Model을 확장해서 만들 IAMDTUser 모델 클래스를 테스트 한다.
@@ -97,12 +97,12 @@ class IAMDTUserModelTestCase(TestCase):
         아이디
         사용여부"""
 
-    def setUp(self):
+    def setUp(self) -> None:
         # 유저 생성
         for user_info in test_user_info:
             user_model.objects.create_user(**user_info)
 
-    def test_create_user(self):
+    def test_info(self) -> None:
         """생성된 정보 확인"""
         user_info = test_user_info[0]
         user = user_model.objects.get(username=user_info["username"])
@@ -126,15 +126,15 @@ class IAMDTUserModelTestCase(TestCase):
         self.assertEqual(user.messenger, user_info["messenger"])
         self.assertEqual(user.messenger_id, user_info["messenger_id"])
 
-    def test_user_model_filter(self):
-        """유저모델 검색 기능 테스트"""
+    def test_filter(self) -> None:
+        """유저모델 검색"""
         # 스태프로 생성된 유저수는 4명
         self.assertEqual(4, user_model.objects.filter(is_staff=True).count())
-
         # 슈퍼유저로 생성된 유저수는 1명
         self.assertEqual(1, user_model.objects.filter(is_superuser=True).count())
 
-        # Role 검색
+    def test_filter_role(self) -> None:
+        """유저모델 role 검색"""
         self.assertEqual(1, user_model.objects.filter(role=user_type.DOCTOR).count())
         self.assertEqual(1, user_model.objects.filter(role=user_type.NURSE).count())
         self.assertEqual(
@@ -146,7 +146,8 @@ class IAMDTUserModelTestCase(TestCase):
             user_model.objects.filter(role=user_type.DEVELOPER).count(),
         )
 
-        # messenger로 검색
+    def test_filter_messenger(self) -> None:
+        """유저모델 messenger 검색"""
         self.assertEqual(
             2,
             user_model.objects.filter(messenger=messenger_type.KAKAOTALK).count(),
@@ -156,33 +157,53 @@ class IAMDTUserModelTestCase(TestCase):
             user_model.objects.filter(messenger=messenger_type.TELEGRAM).count(),
         )
 
-    def test_user_info_validation(self):
-        """유저 정보 변경 검증"""
-        user_info = test_user_info[2]
-        user = user_model.objects.get(username=user_info["username"])
+    def test_validation_role(self) -> None:
+        """유저 role 검증"""
+        user = user_model.objects.first()
 
-        # 전화번호 잘못된 값 지정시 검증 실패
-        user.refresh_from_db()
-        user.phone = "asdf"
-        with self.assertRaises(ValidationError):
-            user.full_clean()
-
-        # role 잘못된 값 지정시 검증 실패
-        user.refresh_from_db()
         user.role = "asdf"
         with self.assertRaises(ValidationError):
             user.full_clean()
 
-        # messenger 잘못된 값 지정시 검증 실패
-        user.refresh_from_db()
+        user.role = None
+        with self.assertRaises(ValidationError):
+            user.full_clean()
+
+    def test_validation_phone(self) -> None:
+        """유저 전화번호 검증"""
+        user = user_model.objects.first()
+
+        user.phone = "asdf"
+        with self.assertRaises(ValidationError):
+            user.full_clean()
+
+        user.phone = ""
+        with self.assertRaises(ValidationError):
+            user.full_clean()
+
+        user.phone = None
+        with self.assertRaises(ValidationError):
+            user.full_clean()
+
+    def test_validation_messenger(self) -> None:
+        """유저 messenger 검증"""
+        user = user_model.objects.first()
+
         user.messenger = "asdf"
         with self.assertRaises(ValidationError):
             user.full_clean()
 
-        # 정상값을 지정하고 메신저 타입이 변경되었는지 확인
-        user.refresh_from_db()
-        user.messenger = messenger_type.TELEGRAM
+        user.messenger = ""
+        with self.assertRaises(ValidationError):
+            user.full_clean()
+
+        user.messenger = None
+        with self.assertRaises(ValidationError):
+            user.full_clean()
+
+        # User.MessengerType.DEFAULT
+        user.messenger = messenger_type.DEFAULT
         user.full_clean()
         user.save()
         user.refresh_from_db()
-        self.assertEqual(user.messenger, messenger_type.TELEGRAM)
+        self.assertEqual(user.messenger, messenger_type.DEFAULT)
