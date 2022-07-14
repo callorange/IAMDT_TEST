5ffa9303bf84974d1cf29298c96f1490be72156f
jeongyeob <sjy20000@gmail.com>
2022-07-14 20:24:27 +0900

    staff 시리얼라이저 추가

---
+++ iamdt_django/iamdt_api/serializers/staff.py
@@ -0,0 +1,33 @@
+"""
+Staff API 관련 Serializer 모듈
+"""
+
+__all__ = [
+    "StaffSerializer",
+]
+
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+class StaffSerializer(serializers.ModelSerializer):
+    """django user serializer"""
+
+    role_display = serializers.CharField(source="get_role_display")
+
+    class Meta:
+        model = get_user_model()
+        fields = [
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "role",
+            "phone",
+            "messenger",
+            "messenger_id",
+        ]
+        read_only_fields = [
+            "id",
+            "username",
+        ]
