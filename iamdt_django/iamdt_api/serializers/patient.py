f1474d752042f024de6755b1842a5214ec8e071e
jeongyeob <sjy20000@gmail.com>
2022-07-15 19:26:07 +0900

    환자 api OAS3 관련 값 리팩토링

--- iamdt_django/iamdt_api/serializers/patient.py
+++ iamdt_django/iamdt_api/serializers/patient.py
@@ -4,60 +4,16 @@ Patient Serializer 모듈
 
 __all__ = ["PatientInfoSerializer"]
 
-from drf_spectacular.utils import extend_schema_serializer, OpenApiExample
+from drf_spectacular.utils import extend_schema_serializer
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 
 from iamdt.models import Patient
+from iamdt_api.scheme.patient import patient_api_examples
 
 
 @extend_schema_serializer(
-    component_name="PatientInfo",
-    examples=[
-        OpenApiExample(
-            name="신규환자1",
-            summary="환자정보 등록 성공",
-            description="보호자 pk가 필요하며 동일 보호자에게 같은 이름의 환자는 등록 불가합니다.",
-            value={"companion": 1, "name": "신규환자"},
-            request_only=True,
-        ),
-        OpenApiExample(
-            name="신규환자2",
-            summary="환자정보 등록 실패(누락)",
-            description="보호자 pk 혹은 이름은 모두 필수 값 입니다.",
-            value={"name": "customer1"},
-            request_only=True,
-            status_codes=["400"],
-        ),
-        OpenApiExample(
-            name="신규환자3",
-            summary="환자정보 등록 실패(중복)",
-            description="보호자 pk + 환자이름 은 중복될 수 없습니다.(테스트시 기존 정보를 확인하세요)",
-            value={"companion": 1, "name": "환자1"},
-            request_only=True,
-            status_codes=["400"],
-        ),
-        OpenApiExample(
-            name="환자정보1",
-            summary="환자정보 조회",
-            description="read_only 필드인 id, created_at, updated_at가 포함 됩니다.",
-            value={
-                "id": "1",
-                "name": "고객1",
-                "phone": "01022223333",
-                "created_at": "2022-07-13T10:28:31.680Z",
-                "updated_at": "2022-07-13T10:30:21.885Z",
-            },
-            response_only=True,
-        ),
-        OpenApiExample(
-            name="환자정보2",
-            summary="환자정보 추가/수정",
-            description="추가/수정시에는 제출된 데이터만 확인용으로 응답됩니다.",
-            value={"name": "고객1", "phone": "01022223333"},
-            response_only=True,
-        ),
-    ],
+    component_name="PatientInfo", examples=patient_api_examples["all"]
 )
 class PatientInfoSerializer(serializers.ModelSerializer):
     """환자정보 시리얼라이저"""
--- iamdt_django/iamdt_api/views/patient.py
+++ iamdt_django/iamdt_api/views/patient.py
@@ -5,23 +5,26 @@ Patient Api View
 __all__ = ["PatientList", "PatientDetail"]
 
 from django.db.models import ProtectedError
-from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import (
-    extend_schema_view,
     extend_schema,
     OpenApiResponse,
-    OpenApiParameter,
-    OpenApiExample,
 )
 from rest_framework import generics, permissions, exceptions
 
 from iamdt.models import Patient
 from iamdt_api.scheme import PAGINATION_QUERY_SCHEME
 from iamdt_api.serializers import PatientInfoSerializer
+from iamdt_api.scheme.patient import patient_api_examples, patient_api_url_param
 
 
-@extend_schema_view(
-    get=extend_schema(
+class PatientList(generics.ListCreateAPIView):
+    """환자 검색/등록 View"""
+
+    permission_classes = [permissions.IsAdminUser]  # is_staff 만
+    queryset = Patient.objects.order_by("-id")
+    serializer_class = PatientInfoSerializer
+
+    @extend_schema(
         tags=["환자"],
         summary="환자 검색",
         description="환자 리스트를 검색합니다",
@@ -30,8 +33,12 @@ from iamdt_api.serializers import PatientInfoSerializer
             403: OpenApiResponse(description="인증 없는 액세스"),
         },
         parameters=PAGINATION_QUERY_SCHEME,
-    ),
-    post=extend_schema(
+        examples=patient_api_examples["read"],
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @extend_schema(
         tags=["환자"],
         summary="환자 등록",
         description="신규 환자 등록합니다. 등록시 보호자가 지정되어야 합니다",
@@ -41,33 +48,22 @@ from iamdt_api.serializers import PatientInfoSerializer
             400: OpenApiResponse(description="잘못된 요청"),
             403: OpenApiResponse(description="인증 없는 액세스"),
         },
-    ),
-)
-class PatientList(generics.ListCreateAPIView):
-    """환자 검색/등록 View"""
+        examples=patient_api_examples["add"],
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
 
-    permission_classes = [permissions.IsAdminUser]  # is_staff 만
-    queryset = Patient.objects.order_by("-id")
+
+class PatientDetail(generics.RetrieveUpdateDestroyAPIView):
+
+    permission_classes = [permissions.IsAdminUser]
+    queryset = Patient.objects.all()
     serializer_class = PatientInfoSerializer
 
+    lookup_url_kwarg = "id"
 
-# 환자 url path kwargs
-url_kwargs = OpenApiParameter(
-    "id",
-    OpenApiTypes.INT,
-    OpenApiParameter.PATH,
-    description="조회할 환자 고유번호",
-    examples=[
-        OpenApiExample(name="환자1", value=1),
-        OpenApiExample(name="환자2", value=2),
-        OpenApiExample(name="환자3", value=3),
-    ],
-)
-
-
-@extend_schema_view(
-    get=extend_schema(
-        parameters=[url_kwargs],
+    @extend_schema(
+        parameters=[patient_api_url_param],
         tags=["환자"],
         summary="환자 조회",
         description="환자의 정보를 조회합니다",
@@ -76,9 +72,13 @@ url_kwargs = OpenApiParameter(
             403: OpenApiResponse(description="인증 없는 액세스"),
             404: OpenApiResponse(description="찾을 수 없는 데이터"),
         },
-    ),
-    put=extend_schema(
-        parameters=[url_kwargs],
+        examples=patient_api_examples["read"],
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @extend_schema(
+        parameters=[patient_api_url_param],
         tags=["환자"],
         summary="환자 수정(put)",
         description="정보를 수정합니다. 동행인 정보에 주의하세요",
@@ -88,9 +88,13 @@ url_kwargs = OpenApiParameter(
             403: OpenApiResponse(description="인증 없는 액세스"),
             404: OpenApiResponse(description="찾을 수 없는 데이터"),
         },
-    ),
-    patch=extend_schema(
-        parameters=[url_kwargs],
+        examples=patient_api_examples["mod"],
+    )
+    def put(self, request, *args, **kwargs):
+        return super().put(request, *args, **kwargs)
+
+    @extend_schema(
+        parameters=[patient_api_url_param],
         tags=["환자"],
         summary="환자 수정(patch)",
         description="정보를 수정합니다. 동행인 정보에 주의하세요",
@@ -100,9 +104,13 @@ url_kwargs = OpenApiParameter(
             403: OpenApiResponse(description="인증 없는 액세스"),
             404: OpenApiResponse(description="찾을 수 없는 데이터"),
         },
-    ),
-    delete=extend_schema(
-        parameters=[url_kwargs],
+        examples=patient_api_examples["mod"],
+    )
+    def patch(self, request, *args, **kwargs):
+        return super().patch(request, *args, **kwargs)
+
+    @extend_schema(
+        parameters=[patient_api_url_param],
         tags=["환자"],
         summary="환자 삭제",
         description="지정된 환자 정보룰 삭제합니다.(해당 환자의 접수내역등이 있다면 오류가 발생합니다.)",
@@ -113,19 +121,10 @@ url_kwargs = OpenApiParameter(
             404: OpenApiResponse(description="찾을 수 없는 데이터"),
             406: OpenApiResponse(description="데이터 보호를 위해 삭제 불가"),
         },
-    ),
-)
-class PatientDetail(generics.RetrieveUpdateDestroyAPIView):
-
-    permission_classes = [permissions.IsAdminUser]
-    queryset = Patient.objects.all()
-    serializer_class = PatientInfoSerializer
-
-    lookup_url_kwarg = "id"
-
-    def perform_destroy(self, instance):
+    )
+    def delete(self, request, *args, **kwargs):
         try:
-            super().perform_destroy(instance)
+            return super().delete(request, *args, **kwargs)
         except ProtectedError as e:
             raise exceptions.NotAcceptable(
                 detail="Unable to delete for data protection", code="protected_data"
